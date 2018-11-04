ffc266a52bdbbe6920d415c867d2fda30eba8774
Nathaniel Wesley Filardo <nwfilardo@gmail.com>
2018-11-04 17:20:20 +0000

    A few more LFS strings
    Catch up with the changes made to date

--- ctfws-lfs-strings.lua
+++ ctfws-lfs-strings.lua
@@ -2,33 +2,36 @@ local modload = "cron", "cron.entry", "schedule", "unschedule",
   "gpio", "HIGH", "LOW", "ALARM_AUTO",
   "mqtt.socket",
   "math", "floor", "unregister",
-  "error", "self"
+  "error", "self",
+  "crypto.hash", "ws2812.buffer",
+  "max", "rep", "get"
 
 local ctfws = "ctfws", "ctfws_lcd", "ctfws_tmr",
   "setupD", "roundD", "rounds", "startT", "endT", "flagsN", "flagsR", "flagsY",
   "times", "config", "deconfig", "setFlags", "setEndTime",
   "GAME NOT CONFIGURED!",
-  "GAME OVER @ %02d:%02d",
+  "GAME OVER @ %02d:%02d", "GAME OVER",
   "START TIME IN FUTURE",
   "TIME IS UP",
   "CTFWS"
 
 local lcdpreload =
   "define_char",
-  "lcd", "mtmr", "ftmr", "dl_elapsed", "dl_elapsed", "dl_remain", "dl_round",
+  "lcd", "mtmr", "ftmr", "fatmr", "dl_elapsed", "dl_elapsed", "dl_remain", "dl_round",
   "attnState", "reset", "drawTimes", "drawFlags", "drawMessage", "drawFlagsMessage",
-  "%02d:%02d.%d", "%02d.%d", "%d", "%-20s", "%d\000: R=%s Y=%s",
+  "%02d:%02d.%d", "%02d.%d", "%d", "%-20s",
+  "%d\000: R=%s Y=%s", "%d\000: R=%s%s", "%d\000: Y=%s%s",
   " CMUKGB CTFWS TIMER ", "                    ",
   "GAME      :",
   "SETUP     :",
   "START IN  :",
   "GAME END  :",
-  "JAILBREAK :",
   "JB#   %d/%d :",
   "JB# %2d/%2d :"
 
 local init3load =
-  "flg_tmr", "lastMsgTime", "mqttUser", "msg_tmr", "dprint", "mqc",
+  "flg_tmr", "lastMsgTime", "mqttUser", "msg_tmr", "fla_tmr",
+  "dprint", "mqc", "publish",
   "nwfmqtt.conf",
   "* * * * *",
   "ctfws/game/config",
